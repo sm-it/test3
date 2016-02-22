0068a30153dc089112b78dedfa472f4226389790
smit <maxim.smirnov@constant.obninsk.ru>
2016-02-22 17:31:51 +0300

    add func system.out

---
+++ src/com/company/TestClass2.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * Created by Максим on 22.02.2016.
+ */
+public class TestClass2 {
+    private String str2;
+
+    public TestClass2(String str2) {
+        this.str2 = str2;
+    }
+}
